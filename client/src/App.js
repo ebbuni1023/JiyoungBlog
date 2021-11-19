a05ba447cd65e23ccc0befc894bfbabfffa7f52c
Jiyoung Yoon <ebbuni1023@users.noreply.github.com>
2021-11-19 02:30:41 -0400

    update

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,19 @@
-import React, { Fragment, fragment } from 'react'; 
+import React, { Fragment } from 'react'; 
 import './App.css';
+// components
+
+import InputTodo from './component/Todo/InputTodo';
+import ListTodos from './component/Todo/ListTodos';
 
 function App() {
-  return <Fragment></Fragment>;
+  return (
+    <Fragment>
+      <div className = "container">
+        <InputTodo />
+        <ListTodos />
+      </div>
+  </Fragment>
+  );
 }
 
 export default App;
